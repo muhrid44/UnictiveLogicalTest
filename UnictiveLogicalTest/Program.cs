a9607d9ac84c78f484d26e6ed8e8e93f89e719e7
muhrid44 <muhrid44@gmail.com>
2023-01-01 20:53:35 +0700

    Add project files.

---
+++ UnictiveLogicalTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace UnictiveLogicalTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Unictive = "Unictive";
+            string UnictiveMedia = "Unictive Media";
+
+            for (int i = 1; i <= 30; i++)
+            {
+                if (i % 14 == 0 && i % 4 == 0)
+                {
+                    Console.WriteLine(UnictiveMedia);
+                }
+                else if (i % 4 == 0)
+                {
+                    Console.WriteLine(Unictive);
+
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+}
